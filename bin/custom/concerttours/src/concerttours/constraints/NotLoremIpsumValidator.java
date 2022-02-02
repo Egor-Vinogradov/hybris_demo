c00bf3ccbab6386b32d08b0734f4907e6f3d4d58
Egor-Vinogradov <7202928@mail.ru>
2022-02-02 23:44:19 +0300

    Define a Custom Validation Constraint

---
+++ bin/custom/concerttours/src/concerttours/constraints/NotLoremIpsumValidator.java
@@ -0,0 +1,17 @@
+package concerttours.constraints;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class NotLoremIpsumValidator implements ConstraintValidator<NotLoremIpsum, String> {
+
+    @Override
+    public void initialize(NotLoremIpsum notLoremIpsum) {
+
+    }
+
+    @Override
+    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+        return s == null || s.isEmpty() || !s.toLowerCase().startsWith("lorem");
+    }
+}
